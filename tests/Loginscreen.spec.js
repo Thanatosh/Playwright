9989a5e5dc517e4e7ce7b52ed88761f53c4c4d26
Thanatos <Thanatosh@gmail.com>
2024-04-17 12:24:33 +0300

    First Commit (Part 5.17)

---
+++ tests/Loginscreen.spec.js
@@ -0,0 +1,21 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http:localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Joni Hostikka',
+        username: 'JoHo',
+        password: 'salasana'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    const locator = page.getByText('Login Screen')
+    await expect(locator).toBeVisible()
+  })
+})
